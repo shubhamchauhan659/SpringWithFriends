24fa6f746b69341a6b5dc32f975832ecdfd07858
shubham <Shubham Chauhan@DESKTOP-NO45MKH>
2021-09-12 23:03:43 +0530

    changes done

---
+++ src/main/java/com/example/demo/SpringWithFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//java is interesting 
+@SpringBootApplication
+public class SpringWithFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithFriendsApplication.class, args);
+	}
+
+}
